7a03afaf7d0e9b4aff48ffc0a81f8627751bb43e
0xterran <0xterran@protonmail.com>
2022-02-06 16:28:56 -0500

    0.1.0-demo

--- src/types/base.type.ts
+++ src/types/base.type.ts
@@ -1,5 +1,5 @@
 export type ChainIDHex = string
 export type Address = string
-export type SemanticVersion = '0.0.1-sandbox' | '0.0.1-genesis' // @FULL_SENVER
+export type SemanticVersion = '0.0.1-sandbox' | '0.1.0-demo' // @FULL_SENVER
 export type Url = string
 export type ChainIDDecimal = string
