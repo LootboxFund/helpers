44779c3ec0c43828412b917ead79a23aa84468e8
0xterran <0xterran@protonmail.com>
2022-02-21 19:03:02 -0500

    changed from @guildfx to @lootboxfund

--- src/types/base.type.ts
+++ src/types/base.type.ts
@@ -1,5 +1,10 @@
+export type SemanticVersion = '0.0.1-sandbox' | '0.1.0-demo' | '0.2.0-sandbox' | '0.2.0-demo'
+
 export type ChainIDHex = string
-export type Address = string
-export type SemanticVersion = '0.0.1-sandbox' | '0.1.0-demo' // @FULL_SENVER
 export type Url = string
 export type ChainIDDecimal = string
+export type ExternalAddress = string;
+export type ContractAddress = string;
+export type Address = ExternalAddress | ContractAddress;
+export type TicketID = string
+export type ABIGenericInterface = any;
