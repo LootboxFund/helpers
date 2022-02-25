7ab2695da536f46c2b5d5bf38456522f35276e77
0xterran <0xterran@protonmail.com>
2022-02-25 05:54:20 -0500

    Safely moved to 0.2.1-sandbox

--- src/types/base.type.ts
+++ src/types/base.type.ts
@@ -1,16 +1,17 @@
 export type ChainIDHex = string
 export type Url = string
 export type ChainIDDecimal = string
-export type ExternalAddress = string & { readonly _: unique symbol };
-export type ContractAddress = string & { readonly _: unique symbol };
-export type Address = ExternalAddress | ContractAddress;
+export type ExternalAddress = string & { readonly _: unique symbol }
+export type ContractAddress = string & { readonly _: unique symbol }
+export type Address = ExternalAddress | ContractAddress
 export type TicketID = string
-export type ABIGenericInterface = any;
+export type ABIGenericInterface = any
 
-export type SemanticVersion = '0.0.1-sandbox' | '0.1.0-demo' | '0.2.0-sandbox' | '0.2.0-demo'
+export type SemanticVersion = '0.0.1-sandbox' | '0.1.0-demo' | '0.2.0-sandbox' | '0.2.0-demo' | '0.2.1-sandbox'
 export enum SemanticVersionEnum {
   '0.0.1-sandbox' = '0.0.1-sandbox',
   '0.1.0-demo' = '0.1.0-demo',
   '0.2.0-sandbox' = '0.2.0-sandbox',
   '0.2.0-demo' = '0.2.0-demo',
-};
+  '0.2.1-sandbox' = '0.2.1-sandbox',
+}
