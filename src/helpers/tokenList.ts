79427bd803325f458a503c2d236a4f8675d67eed
0xnewton <0xnewton@protonmail.com>
2022-03-27 10:06:14 -0400

    remove some seemingly unused methods + update cdn routes to new bucket conventions

--- src/helpers/tokenList.ts
+++ src/helpers/tokenList.ts
@@ -2,7 +2,7 @@ import { ChainIDHex, Address, SemanticVersion } from '../types'
 
 // builds CDN path for indexing tokens
 export const buildTokenIndexCDNRoutes = ({ chainIdHex }: { semver: SemanticVersion; chainIdHex: ChainIDHex }) => {
-  const cdnTokenIndexPath = `v/${chainIdHex}/tokens/index.json`
+  const cdnTokenIndexPath = `tokens/${chainIdHex}/index.json`
   return cdnTokenIndexPath
 }
 
@@ -15,30 +15,11 @@ export const buildTokenCDNRoute = ({
   semver: SemanticVersion
   address: Address
 }) => {
-  const cdnTokenPath = `v/${chainIdHex}/tokens/${address}.json`
+  const cdnTokenPath = `tokens/${chainIdHex}/${address}.json`
   return cdnTokenPath
 }
 
-// builds CDN path for crowdsales
-export const buildCrowdSaleCDNRoute = ({
-  chainIdHex,
-  address,
-}: {
-  chainIdHex: ChainIDHex
-  semver: SemanticVersion
-  address: Address
-}) => {
-  const cdnTokenPath = `v/${chainIdHex}/crowdsales/${address}.json`
-  return cdnTokenPath
-}
-
-// builds CDN path for indexing crowdsales
-export const buildCrowdsaleIndexCDNRoutes = ({ chainIdHex }: { semver: SemanticVersion; chainIdHex: ChainIDHex }) => {
-  const cdnTokenIndexPath = `v/${chainIdHex}/crowdsales/index.json`
-  return cdnTokenIndexPath
-}
-
-// builds full CDN path
-export const buildFullCDNRoute = ({ bucketName, filePath }: { bucketName: string; filePath: string }) => {
-  return `https://firebasestorage.googleapis.com/v0/b/${bucketName}/o/${filePath}?alt=media`
-}
+// // builds full CDN path
+// export const buildFullCDNRoute = ({ bucketName, filePath }: { bucketName: string; filePath: string }) => {
+//   return `https://firebasestorage.googleapis.com/v0/b/${bucketName}/o/${filePath}?alt=media`
+// }
