f78be07ac66e8fc8460685e678597c30ac3b82d3
0xnewton <0xnewton@protonmail.com>
2022-09-25 14:38:12 -0400

    add contract abi snippets

---
+++ src/constants/abi.ts
@@ -0,0 +1,6 @@
+/**
+ * Event ABIs for Cosmic Lootbox
+ */
+
+export const LootboxCreated = 'LootboxCreated(string,address,address,uint256,string)'
+export const MintTicket = 'MintTicket(address,address,uint256,uint256)'
