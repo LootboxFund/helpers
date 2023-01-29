fee8cd1e2a17f2bef19fe5ecb4c2eb2f26252ee1
0xnewton <0xnewton@protonmail.com>
2023-01-29 00:59:26 -0500

    adds helper for lootbox edit perms

--- src/helpers/lootbox.ts
+++ src/helpers/lootbox.ts
@@ -1,5 +1,9 @@
-import { LootboxType, Lootbox_Firestore } from '../types'
+import { LootboxType, Lootbox_Firestore, UserID } from '../types'
 
 export const isLootboxClaimsExcludedFromEventLimits = (lootbox: Lootbox_Firestore) => {
   return lootbox?.type === LootboxType.Promoter
+}
+
+export const doesUserHaveLootboxEditPermission = (lootbox: Lootbox_Firestore, userID: UserID) => {
+  return lootbox?.creatorID === userID || lootbox?.createdOnBehalfOf === userID
 }
