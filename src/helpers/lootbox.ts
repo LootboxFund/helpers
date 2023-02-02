1853227ba802376f22f2bb0a4f9e3eda7148a136
0xnewton <0xnewton@protonmail.com>
2023-02-02 12:56:45 -0500

    fix widget build issue with ? syntax bullllllSHIT

--- src/helpers/lootbox.ts
+++ src/helpers/lootbox.ts
@@ -1,9 +1,15 @@
 import { LootboxType, Lootbox_Firestore, UserID } from '../types'
 
-export const isLootboxClaimsExcludedFromEventLimits = (lootbox: Lootbox_Firestore) => {
-  return lootbox?.type === LootboxType.Promoter
+export const isLootboxClaimsExcludedFromEventLimits = (lootbox: Lootbox_Firestore): boolean => {
+  if (!lootbox || !lootbox.type) {
+    return false
+  }
+  return lootbox.type === LootboxType.Promoter
 }
 
-export const doesUserHaveLootboxEditPermission = (lootbox: Lootbox_Firestore, userID: UserID) => {
-  return lootbox?.creatorID === userID || lootbox?.createdOnBehalfOf === userID
+export const doesUserHaveLootboxEditPermission = (lootbox: Lootbox_Firestore, userID: UserID): boolean => {
+  if (!userID || !lootbox) {
+    return false
+  }
+  return lootbox.creatorID === userID || lootbox.createdOnBehalfOf === userID
 }
